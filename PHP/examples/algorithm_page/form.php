ce8aa68c1af9ac5ac9513257931b156d73b9adf8
panayiotis <pastosdev@gmail.com>
2015-12-24 02:18:40 +0200

    Minor formatting on example

--- PHP/examples/algorithm_page/form.php
+++ PHP/examples/algorithm_page/form.php
@@ -7,5 +7,6 @@ $classifier = new MammalClassifier();
 
 if(isset($_POST['data'])){
     $classifications = json_decode($_POST['data'],true);
+    echo "\nResult: \n";
     print_r($classifier->onDataSet($classifications)->classify()->getResult());
 }
