6d9dd5d0dc5d6c53e3ae0771ff4b1f6f9e6305bf
panayiotis <pastosdev@gmail.com>
2015-12-24 02:17:23 +0200

    Added demonstration of nothing_here

--- PHP/examples/algorithm_page/js/prefill.js
+++ PHP/examples/algorithm_page/js/prefill.js
@@ -1,94 +1,97 @@
 var elem = [
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 2
-        },
-        {
-            "cat": 2
-        },
-        {
-            "buffalo": 1
-        },
-        {
-            "cat": 2
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 2
-        },
-        {
-            "giraffe": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 2,
-            "antelope": 1
-        },
-        {
-            "buffalo": 2,
-            "antelope": 1
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "buffalo": 1,
-            "antelope": 1
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "antelope": 2
-        },
-        {
-            "antelope": 2
-        }
-    ];
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 2
+    },
+    {
+        "cat": 2
+    },
+    {
+        "buffalo": 1
+    },
+    {
+        "cat": 2
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 1,
+        "antelope": 2
+    },
+    {
+        "giraffe": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 2,
+        "antelope": 1
+    },
+    {
+        "buffalo": 2,
+        "antelope": 1
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "nothing_here": 0
+    },
+    {
+        "buffalo": 1,
+        "antelope": 1
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "antelope": 2
+    },
+    {
+        "antelope": 2
+    }
+];
 
-$(function(){
-	$('#data').val(JSON.stringify(elem));
+$(function () {
+    $('#data').val(JSON.stringify(elem));
 });
