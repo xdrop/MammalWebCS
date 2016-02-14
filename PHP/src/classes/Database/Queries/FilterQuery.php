8ffc7fc4e9b382ae0eaf0c4f8494a7b86662255f
panayiotis <pastosdev@gmail.com>
2016-02-14 21:56:49 +0000

    Including FilterQuery in core dependencies

--- PHP/src/classes/Database/Queries/FilterQuery.php
+++ PHP/src/classes/Database/Queries/FilterQuery.php
@@ -2,14 +2,19 @@
 	class FilterQuery extends Query
 	{
 		protected function fetchQuery(&$params){
+
+            if (!Utils::keysExist(['species1','species2'], $params)) {
+                throw new BadMethodCallException("You need to provide a value to species1 and species2 before using this query.");
+            }
+
 			/* Query
 				SELECT (id, photo_id) FROM classified
 					WHERE 'species' = 20 OR 'species' = 22 (The parameter provided)
 			 */
 
 			$query = $this->db->from('classified')
-						 ->select('id') 
-						 ->select('photo_id') 
+						 ->select('id')
+						 ->select('photo_id')
 						 ->where("species = " . $params["species1"] . " OR " . "species = " . $params["species2"]); // assuming whoever calls this passes a species1 and species2 in the params array
 
 			/* Add the query */
