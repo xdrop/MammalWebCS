650c40435ae8f1d25f083e12b1094e4190d14eff
ehockedy <edhockedy@hotmail.com>
2016-02-19 17:12:33 +0000

    front end can run runQuery.php for filtering species

--- PHP/src/classes/Database/Queries/runQuery.php
+++ PHP/src/classes/Database/Queries/runQuery.php
@@ -1,6 +1,8 @@
 <?php
 require('../../../core.php');
 
+$query = $_GET["query"];
+//$query = json_decode($query, true);
 //$myQuery = new FilterQuery();
 //
 //$results = $myQuery->with(["species1" => 20, "species2" => 22])->fetch();
@@ -9,6 +11,14 @@ require('../../../core.php');
 
 $newQueryTest = new SpeciesFilterQuery();
 
-$results = $newQueryTest->with(["no_of_classifications" => 10])->fetchCSV();
+$results = $newQueryTest->with(["species_include" => $query])->fetch();
+
+//$newNameQuery = new HabitatNameQuery();
+
+//$results = $newNameQuery->fetch();
+//$results = $newQueryTest->with([""])->fetchCSV();
 //currently just print for test purposes
-print($results);
+//print_r($results);
+echo json_encode(array("filterResults" => $results));
+
+?>
