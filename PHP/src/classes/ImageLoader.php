308084bfe9a4bb85ced0e261553863ad98285fc2
panayiotis <pastosdev@gmail.com>
2016-03-02 00:29:54 +0000

    URLs to actual images can now be returned

---
+++ PHP/src/classes/ImageLoader.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class ImageLoader
+{
+
+    public static function getURLFromId($imageId)
+    {
+        $query = new ImageInformationQuery();
+        $results = $query->with(['imageId' => $imageId])->fetch();
+        $personId = $results['person_id'];
+        $siteId = $results['site_id'];
+        $filename = $results['filename'];
+        return self::getURL($personId,$siteId,$filename);
+    }
+
+    public static function getURL($personId, $siteId, $filename)
+    {
+        return 'http://www.mammalweb.org/biodivimages/person_' . $personId . '/site_' . $siteId . '/' . $filename;
+    }
+}
