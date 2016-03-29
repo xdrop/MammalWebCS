8e2a7c058906a2f8716ee90ca26a0569ded213ac
ehockedy <edhockedy@hotmail.com>
2016-03-30 00:40:10 +0100

    Fixed "last page" bug

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -216,7 +216,7 @@ function lastPage()
 		}
 		else //filterResults is a multiple of resPerPage. Need this stop a blank last page.
 		{
-			resStart = filterResults.length - (filterResults.length/resPerPage - 1);
+			resStart = filterResults.length - resPerPage;
 		}
 		displayTable(filterResults);
 	}
