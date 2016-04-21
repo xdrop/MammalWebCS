7e98dd93c0e415661916dd0c96951817a7ba7237
ehockedy <edhockedy@hotmail.com>
2016-04-21 02:34:15 +0100

    Added scientist filter

--- frontend/js/indexScientist.js
+++ frontend/js/indexScientist.js
@@ -1,16 +1,27 @@
 /* TO DO:
 	- Make filter buttons either not searchable (preferred), or make search work 
 */
+/*
+WHEN ADDING A NEW FILTER CATEGORY MUST ADD DATA AT MULTIPLE PLACES - THEY ARE NUMBERED LIKE THIS (1) 
+*/
 
-
-//FILTER CRITERIA
+//FILTER CRITERIA (1)
 var filters = {}; //Stores the filters to be applied. The different filters are ANDed together i.e. badger AND forest. Within filter they are ORed e.g. badger OR fox
 var species_include = []; //Stores the species to be included i.e. photos with any of these animals in 
 var species_exclude = []; //Stores the species to be exluded i.e. photos without any of these animals in 
 var habitats = []; //Stores the habitats to be included i.e. photos featuring any of these habitats 
 var sites = []; //Stores the sites to be included i.e. photos from any of these sites 
+var contains_human = false;
+var is_flagged = false;
+var includedUsers = [];
+var excludedUsers = [];
+var numSpecies = 0;
+var minNumClassifications = 0;
+var maxNumClassifications = 0;
+var numClassifications = 0;
 var taken_start = "1970-01-01 00:00:00"; //Must have both dates/time so these are the default values 
 var taken_end = "2100-01-01 00:00:00";
+var scientist_dataset = true;
 
 //RESULTS AND TABLE VARIABLES
 var filterResults = []; //Holds the json of the most recent filter results 
@@ -23,6 +34,7 @@ var currentSort = ""; //The attribute (table heading) currently being sorted
 var iconIndex = 0; //The direction of the icon. 0 is down, 1 is up.
 var icons = ['<i class="dropdown icon"></i>', '<i class="dropdown icon vertically flipped"></i>']; //The two icons
 
+var csv_filename;
 
 //UPDATE THE NUMBER OF ROWS OF THE RESULT TABLE TO BE SHOWN
 //@param value - the number of results to be show. Options are 10, 25, 50, 100, 500 or All 
@@ -100,7 +112,7 @@ function displayTable(json)
 {
 	if(json == "NO RESULT") //No photos for the given criteria 
 	{
-		$("#resultsTable").html("<tr class='center aligned'><td colspan='7' class='align right'><b>No results</b></td></tr>");
+		$("#resultsTable").html("<tr class='center aligned'><td colspan='11' class='align right'><b>No results</b></td></tr>");
 	}
 	else 
 	{
@@ -150,11 +162,14 @@ function displayTable(json)
 			}
 			
 			data += "<td>" + obj.species_name + "</td>"; //Add species 
+			data += "<td>" + obj.contains_human + "</td>"; //Add if it contains human
 			data += "<td>" + obj.time_classified + "</td>"; //Add time classified
 			data += "<td>" + obj.taken + "</td>"; //Add time taken
-			data += "<td>" + obj.habitat_name + "</td>"; //Add habitat
-			data += "<td>" + obj.site_name + "</td>"; //Add site
-
+			data += "<td>" + obj.person_id + "</td>"; //Add person ID
+			data += "<td>" + obj.site_name + "</td>"; //Add site IDs
+			data += "<td>" + obj.habitat_name + "</td>";
+			data += "<td>" + obj.evenness_species + "</td>";
+			data += "<td>" + obj.evenness_count + "</td>";
 			$(data + "</tr>").appendTo("#resultsTable");
 		}
 	}
@@ -211,7 +226,7 @@ function lastPage()
 		if(filterResults.length % resPerPage != 0) //i.e. the length of filterResults = resPerPage. Ths stops it from showing empty last page when showing all
 		{
 			resStart = filterResults.length - (filterResults.length % resPerPage);
-			console.log(resStart, resPerPage, resStart/resPerPage);
+			//console.log(resStart, resPerPage, resStart/resPerPage);
 			document.getElementById("pagesDropdown").options[resStart/resPerPage].setAttribute("selected", true);
 		}
 		else //filterResults is a multiple of resPerPage. Need this stop a blank last page.
@@ -260,9 +275,10 @@ function populatePagesDropdown(currentValue)
 }
 
 
-//APPLY THE FILTER 
+//APPLY THE FILTER (2) 
 $("#applyFilterButton").click(function () //If the filter button is pressed 
 { 
+	filters = {}; //reset filters
 	if(species_include.length != 0)
 	{
 		filters.species_include = species_include; //Add the included species to filters 
@@ -279,6 +295,14 @@ $("#applyFilterButton").click(function () //If the filter button is pressed
 	{
 		filters.site_id = sites;
     }
+	if(contains_human == true)
+	{
+		filters.contains_human = true;
+	}
+	if(is_flagged == true)
+	{
+		filters.flagged = true;
+	}
 	if(taken_start != "")
 	{
 		filters.taken_start = taken_start;
@@ -286,6 +310,34 @@ $("#applyFilterButton").click(function () //If the filter button is pressed
 	if(taken_end != "")
 	{
 		filters.taken_end = taken_end;
+	}
+	if(includedUsers.length != 0)
+	{
+		filters.users_include = includedUsers;
+	}
+	if(excludedUsers.length != 0)
+	{
+		filters.users_exclude = excludedUsers;
+	}
+	if(numSpecies != 0)
+	{
+		filters.no_of_species = numSpecies;
+	}
+	if(numClassifications != 0)
+	{
+		filters.no_of_classifications = numClassifications;
+	}
+	if(minNumClassifications != 0)
+	{
+		filters.no_of_classifications_from = minNumClassifications;
+	}
+	if(maxNumClassifications != 0)
+	{
+		filters.no_of_classifications_to = maxNumClassifications;
+	}
+	if(scientist_dataset != false)
+	{
+		filters.scientist_dataset = true;
 	}
 	//alert(JSON.stringify(filters));
 	$.ajax({
@@ -303,17 +355,27 @@ $("#applyFilterButton").click(function () //If the filter button is pressed
 				tableHeads[i].innerHTML = tableHeads[i].innerHTML.split("<")[0];
 			}
             // TODO: add a link to "filter.php?csv=" + csv_filename which will download the csv output
-			//var csv_filename = json.csv; //Get the csv file name
-            //console.log(csv_filename + "  ");
+			csv_filename = json.csv; //Get and store the csv file name
+			document.getElementById("csvButton").disabled = false;
         },
         error:   function () {
             //alert("It does not work...");
 			filterResults = []; //Store the result as empty 
-			displayTable("NO RESULT"); ////WHY DOES THIS NOT SUCCEED FOR FILTERS WITH NO RESULTS
+			displayTable("NO RESULT");
 			updatePageNum();
         }
     });
-	filters = {}; //reset filters
+	
+});
+
+function downloadCSV()
+{
+	$("#downloadCSVLink").attr("href", "../backend/src/storage/csv/" + csv_filename);
+	return true; //return makes the href wait 
+}
+
+$("#downloadCSVButton").click(function (){
+	
 });
 
 $(document).ready(function () {
@@ -323,7 +385,9 @@ $(document).ready(function () {
     var $habitatDrop = $("#habitatDrop");
     var $siteDrop = $("#siteDrop");
 	
-	$("#resultsTable").html("<tr class='center aligned'><td colspan='7' class='align right'>Results go here...</td></tr>"); //Show were results will go
+	$('.ui.checkbox').checkbox(); //Initialise checkbox
+	
+	$("#resultsTable").html("<tr class='center aligned'><td colspan='11' class='align right'>Results go here...</td></tr>"); //Show were results will go (5)
 	
 	populatePagesDropdown(0); //Make the dropdown have only the value of 0 in. Looks better than a dropdown with nothing 
 	
@@ -358,12 +422,20 @@ $(document).ready(function () {
 	
 	$('.ui.dropdown').dropdown(); //Initialise dropdown 
 
-	//WHEN THERE IS A CHANGE IN THE MAIN DROPDOWN 
+	//WHEN THERE IS A CHANGE IN THE MAIN DROPDOWN (3) 
 	$masterDrop.dropdown({onChange: function(value,text){ 
 			species_include.length = 0; //Resets the array. arr = [] does not work.
 			species_exclude.length = 0;
 			habitats.length = 0;
 			sites.length = 0;
+			contains_human = false;
+			is_flagged = false;
+			includedUsers.length = 0;
+			excludedUsers.length = 0;
+			numSpecies = 0;
+			minNumClassifications = 0;
+			maxNumClassifications = 0;
+			numClassifications = 0;
 			taken_start = "1970-01-01 00:00:00"; //Set dated to default values 
 			taken_end = "2100-01-01 00:00:00";
 			values = $masterDrop.dropdown("get values");
@@ -389,6 +461,14 @@ $(document).ready(function () {
 				{
 					sites.push(parseInt(filterValue));
 				}
+				else if(filterCategory == "contains_human")
+				{
+					contains_human = true;
+				}
+				else if(filterCategory == "is_flagged")
+				{
+					is_flagged = true;
+				}
 				else if(filterCategory == "datetimeFrom")
 				{
 					taken_start = filterValue;
@@ -397,76 +477,91 @@ $(document).ready(function () {
 				{
 					taken_end = filterValue;
 				}
+				else if(filterCategory == "numSpecies")
+				{
+					numSpecies = filterValue;
+				}
+				else if(filterCategory == "numClassifications")
+				{
+					numClassifications = filterValue;
+				}
+				else if(filterCategory == "minNumClassifications")
+				{
+					minNumClassifications = filterValue;
+				}
+				else if(filterCategory == "maxNumClassifications")
+				{
+					maxNumClassifications = filterValue;
+				}
+				else if(filterCategory == "includeUser")
+				{
+					console.log(filterValue);
+					includedUsers.push(parseInt(filterValue));
+				}
+				else if(filterCategory == "excludeUser")
+				{
+					excludedUsers.push(parseInt(filterValue));
+				}
 			}
 		}
 	});
 	
-	//SPECIES INCLUDE
-    $speciesIncludeDrop.dropdown({
-        action: function(text, value) {
-            $masterDrop.dropdown("add value", "animal=" + value, "Include: " + text); //Add the value 
-            $masterDrop.dropdown("add label", "animal=" + value, "Include: " + text,"green"); //Add the label 
-            //$masterDrop.dropdown("set selected", value);
-            $speciesIncludeDrop.dropdown("action hide");
-        },
-        fields:      {name: "name", value: "id"},
-        apiSettings: {
-            url:        '../backend/src/api/internal/list.php?item=species',
-            onResponse: function (response) {
-                return {success: true, results: response};
-            }
-        }
-    });
+	var usesApi = ["speciesIncludeDrop", "speciesExcludeDrop", "habitatDrop", "siteDrop"]; //The filters that need to be populaed by an api
 	
-	//SPECIES EXCLUDE
-	$speciesExcludeDrop.dropdown({
-        action: function(text, value) {
-            $masterDrop.dropdown("add value", "no_animal=" + value, "Exclude: " + text);
-            $masterDrop.dropdown("add label", "no_animal=" + value, "Exclude: " + text,"red");
-            //$masterDrop.dropdown("set selected", value);
-            $speciesExcludeDrop.dropdown("action hide");
-        },
-        fields:      {name: "name", value: "id"},
-        apiSettings: {
-            url:        '../backend/src/api/internal/list.php?item=species',
-            onResponse: function (response) {
-                return {success: true, results: response};
-            }
-        }
-    });
-	
-	//HABITAT
-    $habitatDrop.dropdown({
-        action: function(text, value) {
-            $masterDrop.dropdown("add value", "habitat=" + value, "Habitat: " + text);
-            $masterDrop.dropdown("add label", "habitat=" + value, "Habitat: " + text.substr(0,text.indexOf(' ')),"blue");
-            $masterDrop.dropdown("set selected", value);
-            $habitatDrop.dropdown("action hide");
-        },
-        fields:      {name: "name", value: "id"},
-        apiSettings: {
-            url:        '../backend/src/api/internal/list.php?item=habitats',
-            onResponse: function (response) {
-                return {success: true, results: response};
-            }
-        }
-    });
+	//The information needed for each dropdown (4) 
+	var info = {
+		"speciesIncludeDrop": ["animal", "Include species", "green", "species"],
+		"speciesExcludeDrop": ["no_animal", "Exclude species", "red", "species"],
+		"habitatDrop": ["habitat", "Habitat", "teal", "habitats"],
+		"siteDrop":["site", "Site", "yellow", "sites"],
+		"humanCheck":["contains_human", "Contains human", "olive"],
+		"flaggedCheck":["is_flagged", "Flagged", "orange"],
+		"numSpecies":["numSpecies", "Number of species", "black"],
+		"numClassifications":["numClassifications", "Number of classifications", "violet"],
+		"minNumClassifications":["minNumClassifications", "Minimum number of classifications", "brown"],
+		"maxNumClassifications":["maxNumClassifications", "Maximum number of classifications", "yellow"],
+		"includeUser":["includeUser", "Included user", "green"],
+		"excludeUser":["excludeUser", "Excluded user", "red"]
+	};
 
-	//SITE
-    $siteDrop.dropdown({
-        action: function(text, value) {
-            $masterDrop.dropdown("add value", "site=" + value, "Site: " + text);
-            $masterDrop.dropdown("add label", "site=" + value, "Site: " + text, "yellow");
-            $masterDrop.dropdown("set selected", value);
-            $habitatDrop.dropdown("action hide");
+	//Add dropdown selection to main selection, and populate dropdown with api 
+	$(".filterOpt").dropdown({
+        action: function(text, value) {	//When an option from the dropdown is chosen
+			var chosenDropdown = event.target.parentElement.parentElement.id //The dropdown that has been chosen. Got by looking through parents of the item chosen from the dropdown 
+			var filterType = info[chosenDropdown][0] + "=";
+			var labelName = info[chosenDropdown][1];
+			var colour = info[chosenDropdown][2];
+            $masterDrop.dropdown("add value", filterType + value)//, labelName + ": " + text); //Add the value 
+			if(chosenDropdown == "habitatDrop") //Only show the first word of the habitat
+			{
+				$masterDrop.dropdown("add label", filterType + value, labelName + ": " + text.substr(0,text.indexOf(' ')), colour);
+			}
+			else
+			{
+				$masterDrop.dropdown("add label", filterType + value, labelName + ": " + text, colour); //Add the label 
+            }
+			//$speciesIncludeDrop.dropdown("action hide");
         },
         fields:      {name: "name", value: "id"},
-        apiSettings: {
-            url:        '../backend/src/api/internal/list.php?item=sites',
-            onResponse: function (response) {
+		apiSettings: {
+			beforeSend: function(settings) {
+				if(usesApi.indexOf(event.target.parentNode.id) != -1)
+				{
+					console.log(settings.url);
+					settings.url = '../backend/src/api/internal/list.php?item=' + info[event.target.parentNode.id][3]; //used instead of the url parameter because cannot seem to reference DOM element to get necessary api query (species, habitats etc.)
+					return settings;
+				}
+				else
+				{
+					return false;
+				}
+				
+			},
+			//url: "../backend/src/api/internal/list.php?",
+			onResponse: function (response) {
                 return {success: true, results: response};
             }
-        }
+		}
     });
 	
 	var prevDateTimeFrom = ""; //Stores the most recent datetime. Used so can replace datetime if a new one decided.
@@ -491,7 +586,7 @@ $(document).ready(function () {
 		$masterDrop.dropdown("remove value", "datetimeFrom=" + prevDateTimeFrom);
 		//Add new datetime
 		$masterDrop.dropdown("add value", "datetimeFrom=" + datetime, "Date from: " + datetime);
-		$masterDrop.dropdown("add label", "datetimeFrom=" + datetime, "Date from: " + datetime, "purple");
+		$masterDrop.dropdown("add label", "datetimeFrom=" + datetime, "Date from: " + datetime, "pink");
 		prevDateTimeFrom = datetime;
 	});
 	
@@ -513,8 +608,68 @@ $(document).ready(function () {
 		$masterDrop.dropdown("remove label", "datetimeTo=" + prevDateTimeTo);
 		$masterDrop.dropdown("remove value", "datetimeTo=" + prevDateTimeTo);
 		$masterDrop.dropdown("add value", "datetimeTo=" + datetime, "Date to: " + datetime);
-		$masterDrop.dropdown("add label", "datetimeTo=" + datetime, "Date to: " + datetime, "orange");
+		$masterDrop.dropdown("add label", "datetimeTo=" + datetime, "Date to: " + datetime, "purple");
 		prevDateTimeTo = datetime;
 	});
+	
+	
+	var prevValues = {
+		"numSpecies":"",
+		"numClassifications":"",
+		"minNumClassifications":"",
+		"maxNumClassifications":""
+	};
+		
+	$(".filterForm").submit(function( event ) {
+		event.preventDefault();
+		//console.log(event.target.children[0].value);
+		var chosenCheck = event.target.id //The dropdown that has been chosen. Got by looking through parents of the item chosen from the dropdown 
+		var enteredValue = event.target.children[0].value;
+		var filterType = info[chosenCheck][0] + "=";
+		var labelName = info[chosenCheck][1];
+		var colour = info[chosenCheck][2];
+		if(prevValues.hasOwnProperty(chosenCheck) == true) //Remve any labels already there 
+		{
+			if(prevValues[chosenCheck] != "")
+			{
+				$masterDrop.dropdown("remove label", prevValues[chosenCheck]);
+				$masterDrop.dropdown("remove value", prevValues[chosenCheck]);
+			}
+		}
+        $masterDrop.dropdown("add value", filterType + enteredValue); //Add the value 
+		$masterDrop.dropdown("add label", filterType + enteredValue, labelName + ": " + enteredValue, colour); //Add the label 
+		if(prevValues.hasOwnProperty(chosenCheck) == true) //Store the values so any values already there are known so dows not put more than one label for categories that are not allowed
+		{
+			prevValues[chosenCheck] = filterType + enteredValue;
+		}
+		event.target.children[0].value = ""; //Reset the form
+	});
+	
+	
+	$(".check").checkbox({
+		onChecked: function(text, value) {
+			var chosenCheck = event.target.id //The dropdown that has been chosen. Got by looking through parents of the item chosen from the dropdown 
+			var filterType = info[chosenCheck][0] + "=";
+			var labelName = info[chosenCheck][1];
+			var colour = info[chosenCheck][2];
+            $masterDrop.dropdown("add value", filterType + "1"); //Add the value 
+			$masterDrop.dropdown("add label", filterType + "1", labelName + ": " + "yes", colour); //Add the label 
+		},
+		onUnchecked: function(text, value){
+			var chosenCheck = event.target.id 
+			var filterType = info[chosenCheck][0] + "=";
+			var labelName = info[chosenCheck][1];
+			var colour = info[chosenCheck][2];
+			$masterDrop.dropdown("remove value", filterType + "1"); //Rremove the value 
+			$masterDrop.dropdown("remove label", filterType + "1", labelName + ": " + "yes", colour); //Remove the label
+		}
+	});
+	//$('.test.checkbox').checkbox('attach events', '.toggle.button');
+	$("#scientistCheck").checkbox({
+		onChange: function(text, value){
+			console.log("OK");
+			scientist_dataset = !scientist_dataset;
+		}
+	})
 	
 });
