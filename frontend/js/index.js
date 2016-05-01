bd50c29b68ad531d29595bfd5e254b1a2b64c931
Ben Hazelwood <benhazelwood@outlook.com>
2016-05-01 16:53:16 +0100

    slidshow

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -94,7 +94,7 @@ $("#slideshowButton").click(function(){
 
 function slideshow(){
     $("#resultsTable:eq(0) tr").each(function(){
-      alert( $(this).html() );
+      alert( $(this).html().attr('href') );
     });
     $('#slide').append("<li><img src=\'" + imageSrc +"\' /></li>");
 
